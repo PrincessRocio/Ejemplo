cdec075ab3b0cd3d62ce324b146c641dc13efbb9
Rocio Vallejo <127776957+PrincessRocio@users.noreply.github.com>
2023-08-22 16:45:28 -0300

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+
+
+#incluide <iostream>
+using namespace std;
+int main (){
+string nombre;
+string apellido;
+cout << "Ingrese su nombre" << endl;
+cin >> nombre >> endl;
+cout << "Ingrese su apellido" << endl;
+cin >> apellido >> endl;
+ cout << "El nombre completi ingresado es: " << nombre << apellido << endl;
+ 
+}
